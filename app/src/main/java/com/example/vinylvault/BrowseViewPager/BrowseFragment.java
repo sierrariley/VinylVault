852ba7303b0b920f59941ec21651e604b1be4bb5
Sage Lawrence <w0811501@myscc.ca>
2024-03-05 15:30:14 -0500

    ViewPager Host Created Minor file edits #6

--- app/src/main/java/com/example/vinylvault/BrowseViewPager/BrowseFragment.java
+++ app/src/main/java/com/example/vinylvault/BrowseViewPager/BrowseFragment.java
@@ -1,4 +1,4 @@
-package com.example.vinylvault;
+package com.example.vinylvault.BrowseViewPager;
 
 import android.os.Bundle;
 
@@ -8,6 +8,8 @@ import android.view.LayoutInflater;
 import android.view.View;
 import android.view.ViewGroup;
 
+import com.example.vinylvault.R;
+
 /**
  * A simple {@link Fragment} subclass.
  * Use the {@link BrowseFragment#newInstance} factory method to
@@ -15,28 +17,21 @@ import android.view.ViewGroup;
  */
 public class BrowseFragment extends Fragment {
 
-    // TODO: Rename parameter arguments, choose names that match
-    // the fragment initialization parameters, e.g. ARG_ITEM_NUMBER
     private static final String ARG_PARAM1 = "param1";
     private static final String ARG_PARAM2 = "param2";
 
-    // TODO: Rename and change types of parameters
     private String mParam1;
     private String mParam2;
 
-    public BrowseFragment() {
-        // Required empty public constructor
-    }
+    public BrowseFragment() {}
 
     /**
      * Use this factory method to create a new instance of
      * this fragment using the provided parameters.
-     *
      * @param param1 Parameter 1.
      * @param param2 Parameter 2.
      * @return A new instance of fragment BrowseFragment.
      */
-    // TODO: Rename and change types and number of parameters
     public static BrowseFragment newInstance(String param1, String param2) {
         BrowseFragment fragment = new BrowseFragment();
         Bundle args = new Bundle();
