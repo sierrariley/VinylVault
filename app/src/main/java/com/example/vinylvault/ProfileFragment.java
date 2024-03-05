84b4892214e35a71f5b7cac06a1c497824191e1d
Sage Lawrence <w0811501@myscc.ca>
2024-03-05 15:00:50 -0500

    #5 - ProfileFragment Updated

--- app/src/main/java/com/example/vinylvault/ProfileFragment.java
+++ app/src/main/java/com/example/vinylvault/ProfileFragment.java
@@ -9,18 +9,17 @@ import android.view.View;
 import android.view.ViewGroup;
 
 /**
+ * Used for the ProfilePage - aka HomePage
  * A simple {@link Fragment} subclass.
  * Use the {@link ProfileFragment#newInstance} factory method to
  * create an instance of this fragment.
  */
 public class ProfileFragment extends Fragment {
 
-    // TODO: Rename parameter arguments, choose names that match
     // the fragment initialization parameters, e.g. ARG_ITEM_NUMBER
     private static final String ARG_PARAM1 = "param1";
     private static final String ARG_PARAM2 = "param2";
 
-    // TODO: Rename and change types of parameters
     private String mParam1;
     private String mParam2;
 
@@ -36,7 +35,6 @@ public class ProfileFragment extends Fragment {
      * @param param2 Parameter 2.
      * @return A new instance of fragment ProfileFragment.
      */
-    // TODO: Rename and change types and number of parameters
     public static ProfileFragment newInstance(String param1, String param2) {
         ProfileFragment fragment = new ProfileFragment();
         Bundle args = new Bundle();
