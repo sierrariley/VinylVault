92f393945a942f46e6dc7028cf1796b3e6e894c6
Sage Lawrence <w0811501@myscc.ca>
2024-04-03 12:30:22 -0400

    Added DB to AddAnAlbum

--- app/src/main/java/com/example/vinylvault/AddAnAlbumFragment.java
+++ app/src/main/java/com/example/vinylvault/AddAnAlbumFragment.java
@@ -1,12 +1,15 @@
 package com.example.vinylvault;
 
+import android.content.Context;
 import android.os.Bundle;
 
 import androidx.fragment.app.Fragment;
+import androidx.navigation.Navigation;
 
 import android.view.LayoutInflater;
 import android.view.View;
 import android.view.ViewGroup;
+import android.view.inputmethod.InputMethodManager;
 import android.widget.Button;
 import android.widget.EditText;
 import android.widget.ImageView;
@@ -15,7 +18,9 @@ import android.widget.RadioGroup;
 import android.widget.SeekBar;
 import android.widget.TextView;
 
+import com.example.vinylvault.Database.AlbumDatabase;
 import com.example.vinylvault.Pojo.Album;
+import com.squareup.picasso.Picasso;
 
 /**
  * TODO: Opens based on fab button, If album status and fab button is hit...
@@ -29,11 +34,11 @@ public class AddAnAlbumFragment extends Fragment {
     public static final String ALBUM = "album";
 
     Album album;
+    int albumStatus;
     ImageView image;
     TextView artist, albumName, genre;
     SeekBar seekBar;
     EditText review;
-    RadioGroup radioGroup;
     RadioButton currentlyListening, done, toListen;
     Button submit;
 
@@ -48,40 +53,60 @@ public class AddAnAlbumFragment extends Fragment {
         genre = view.findViewById(R.id.add_album_genre);
         seekBar = view.findViewById(R.id.add_album_seek_bar);
         review = view.findViewById(R.id.add_album_review);
-        radioGroup = view.findViewById(R.id.add_album_radio_group);
         currentlyListening = view.findViewById(R.id.add_album_radio1);
         toListen = view.findViewById(R.id.add_album_radio2);
         done = view.findViewById(R.id.add_album_radio3);
         submit = view.findViewById(R.id.add_album_button);
 
-        switch (radioGroup.getCheckedRadioButtonId()) {
-            case 0: //currently listening
-                break;
-            case 1: //to listen
-                break;
-            case 2: //done
-                break;
-        }
-
-
-        if (done.isChecked()) {
-            //do this
-        } else if (currentlyListening.isChecked()) {
-            //do this instead
-        }
-
-
         if (getArguments() != null) {
             if (getArguments().getInt(ACTION_TYPE) == UPDATE) {
                 album = getArguments().getParcelable(ALBUM);
-                //Update Member (update database)
+                Picasso.get().load(album.getArtwork()).error(R.drawable.album_error_placeholder).into(image);
+                artist.setText(album.getArtistName());
+                albumName.setText(album.getName());
+                genre.setText(album.getGenre());
+                seekBar.setProgress(album.getRating());
+//                review.setText(album.getReview()); TODO: Fix once Database has this implemented
+                submit.setText("Update Album");
 
             } else if (getArguments().getInt(ACTION_TYPE) == CREATE) {
                 album = new Album();
-                //Create a new one (save to database)
+                submit.setText("Add Album");
             }
         }
 
+        submit.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View view) {
+                //dismiss the keyboard
+                if(view.requestFocus()){
+                    InputMethodManager inputMethodManager =
+                            (InputMethodManager) getContext().getSystemService(Context.INPUT_METHOD_SERVICE);
+                    inputMethodManager.hideSoftInputFromWindow(view.getWindowToken(), 0);
+                }
+
+                if (done.isChecked()) {
+                    album.setStatus(3);
+                } else if (currentlyListening.isChecked()) {
+                    album.setStatus(1);
+                } else if (toListen.isChecked()) {
+                    album.setStatus(2);
+                }
+
+                album.setRating(seekBar.getProgress());
+//                album.setReview(reivew.getText());
+
+                AlbumDatabase db = new AlbumDatabase(getContext());
+                if (getArguments().getInt(ACTION_TYPE) == UPDATE) {
+                    db.updateAlbum(album);
+                } else if (getArguments().getInt(ACTION_TYPE) == CREATE) {
+                    db.addAlbum(album);
+                }
+                db.close();
+
+                Navigation.findNavController(view).popBackStack();
+            }
+        });
 
         return view;
     }
