d1a35079a8802efb48a215f34e004722fa35cce7
Sierra Riley <w0791053@myscc.ca>
2024-04-07 21:59:27 -0400

    #34 Fab button implementes

--- app/src/main/java/com/example/vinylvault/AddAnAlbumFragment.java
+++ app/src/main/java/com/example/vinylvault/AddAnAlbumFragment.java
@@ -8,6 +8,8 @@ import android.view.LayoutInflater;
 import android.view.View;
 import android.view.ViewGroup;
 
+import com.example.vinylvault.Pojo.Album;
+
 /**
  * A simple {@link Fragment} subclass.
  * Use the {@link AddAnAlbumFragment#newInstance} factory method to
@@ -55,6 +57,12 @@ public class AddAnAlbumFragment extends Fragment {
         }
     }
 
+    public static final int UPDATE = 1;
+    public static final int CREATE= 2;
+    public static final String ALBUM = "album";
+    public static final String ACTION_TYPE = "action_type";
+
+    Album album;
     @Override
     public View onCreateView(LayoutInflater inflater, ViewGroup container,
                              Bundle savedInstanceState) {
--- app/src/main/java/com/example/vinylvault/MainActivity.java
+++ app/src/main/java/com/example/vinylvault/MainActivity.java
@@ -1,14 +1,19 @@
 package com.example.vinylvault;
 
+import android.content.Intent;
+import android.net.Uri;
 import android.os.Bundle;
 import android.view.Menu;
 import android.view.MenuItem;
+import android.view.View;
 
 import com.google.android.material.bottomnavigation.BottomNavigationView;
 
 import androidx.annotation.NonNull;
+import androidx.annotation.Nullable;
 import androidx.appcompat.app.AppCompatActivity;
 import androidx.navigation.NavController;
+import androidx.navigation.NavDestination;
 import androidx.navigation.Navigation;
 import androidx.navigation.ui.AppBarConfiguration;
 import androidx.drawerlayout.widget.DrawerLayout;
@@ -28,10 +33,30 @@ public class MainActivity extends AppCompatActivity {
         binding = ActivityMainBinding.inflate(getLayoutInflater());
         setContentView(binding.getRoot());
 
-
-
-
-
+        //TODO: Implement Fab Button
+        /**
+         * Author: Sierra Riley
+         * This on click listener allows the fab button to navigate to a specific fragment
+         * when it is on a certain fragment
+         */
+        binding.fab.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View v) {
+                NavDestination currentFragment = navController.getCurrentDestination();
+                //If we are on album summary fragment
+                if(currentFragment.getId() == R.id.nav_album_summary){
+                    Bundle extra = new Bundle();
+                    extra.putInt(AddAnAlbumFragment.ACTION_TYPE, AddAnAlbumFragment.CREATE);
+                    //Go to the add album fragment in create mode
+                    navController.navigate(R.id.nav_add_album, extra);
+                }else if(currentFragment.getId() == R.id.nav_search){
+                    String url = "https://www.ticketmaster.ca/discover/concerts?landing=c&awtrc=true&c=SEM_TMBRAND_ggl_6619616063_137379093082_ticketmaster&GCID=0&&gad_source=1&gclid=Cj0KCQjwiMmwBhDmARIsABeQ7xSC8NgZSWyUxtCT6yGxUrflbhNZSAuPKrKFMtZPweWV0XqMij0t3JUaAlNiEALw_wcB&gclsrc=aw.ds";
+                    Uri webPage = Uri.parse(url);
+                    Intent i = new Intent(Intent.ACTION_VIEW, webPage);
+                    startActivity(i);
+                }
+            }
+        });
 
         BottomNavigationView navView = findViewById(R.id.nav_view);
         AppBarConfiguration appBarConfiguration = new AppBarConfiguration.Builder(
@@ -41,13 +66,23 @@ public class MainActivity extends AppCompatActivity {
                 R.id.nav_search)
                 .build();
 
-        //TODO: Implement Fab Button
+
 
         //TODO: Add Animations to RecyclerViews
 
         navController = Navigation.findNavController(this, R.id.nav_host_fragment_activity_main);
         NavigationUI.setupActionBarWithNavController(this, navController, appBarConfiguration);
         NavigationUI.setupWithNavController(binding.navView, navController);
+
+        navController.addOnDestinationChangedListener(new NavController.OnDestinationChangedListener() {
+            @Override
+            public void onDestinationChanged(@NonNull NavController controller, @NonNull NavDestination destination, @Nullable Bundle arguments) {
+                if(destination.getId() == R.id.nav_search){
+                    binding.fab.setImageResource(R.drawable.ic_baseline_screen_search_desktop_24);
+                }
+
+            }
+        });
     }
 
     /**
